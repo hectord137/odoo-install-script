0cd71ea992ca6ea9c740c6950c2400a46e478c6f
Hector Daniel <hectord137@users.noreply.github.com>
2020-07-13 21:57:16 -0400

    Add files via upload

---
+++ install-odoo-hdap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+cd /home/ubuntu
+rm -frd ./*
+mkdir -p /home/ubuntu/odoo-config
+mkdir -p /home/ubuntu/extra-addons
+mkdir -p /home/ubuntu/nginx-data
+mkdir -p /home/ubuntu/nginx-data/ssl
+mkdir -p /home/ubuntu/nginx-data/logs
+
+cd /home/ubuntu/extra-addons
+curl -o master.zip -sSL https://github.com/hectord137/odoo-addons/archive/master.zip
+sudo apt-get install -y unzip
+unzip -o master.zip
+mv -f odoo-addons-master/* ./
+rm -frd master.zip
+
+cd /home/ubuntu
+apt-get remove -y docker docker-engine docker.io containerd runc
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+curl -sSL "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+apt-get update
+apt-get install -y awscli
+
+cd /home/ubuntu/odoo-config
+aws s3 cp s3://odoo-conf/odoo.conf ./
+
+cd /home/ubuntu/nginx-data
+aws s3 cp s3://odoo-conf/default.conf ./
+
+cd /home/ubuntu/nginx-data/ssl
+ aws s3 cp s3://ssl-cert-hdap/ ./ --recursive
+
+cd /home/ubuntu
+aws s3 cp s3://odoo-conf/docker-compose.yml ./
+docker-compose rm -f -s -v
+docker-compose up -d --force-recreate
