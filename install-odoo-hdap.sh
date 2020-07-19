015d7ff26464fdfc80853e25cf69d553ebff8e9f
Hector Daniel <hectord137@users.noreply.github.com>
2020-07-18 22:56:55 -0400

    Update install-odoo-hdap.sh

--- install-odoo-hdap.sh
+++ install-odoo-hdap.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
 cd /home/ubuntu
-#rm -frd ./*
-mkdir -p /home/ubuntu/odoo-config
-mkdir -p /home/ubuntu/extra-addons
-mkdir -p /home/ubuntu/nginx-data
-mkdir -p /home/ubuntu/nginx-data/ssl
-mkdir -p /home/ubuntu/nginx-data/logs
 mkdir -p /home/ubuntu/postgre-data
 
+rm -frd /home/ubuntu/extra-addons
+mkdir -p /home/ubuntu/extra-addons
 cd /home/ubuntu/extra-addons
-rm -frd ./*
 curl -o master.zip -sSL https://github.com/hectord137/odoo-addons/archive/master.zip
 sudo apt-get install -y unzip
 unzip -o master.zip
@@ -28,14 +23,20 @@ chmod +x /usr/local/bin/docker-compose
 apt-get update
 apt-get install -y awscli
 
+rm -frd /home/ubuntu/odoo-config
+mkdir -p /home/ubuntu/odoo-config
 cd /home/ubuntu/odoo-config
 aws s3 cp s3://odoo-conf/odoo.conf ./
 
+rm -frd /home/ubuntu/nginx-data
+mkdir -p /home/ubuntu/nginx-data
+mkdir -p /home/ubuntu/nginx-data/ssl
+mkdir -p /home/ubuntu/nginx-data/logs
 cd /home/ubuntu/nginx-data
 aws s3 cp s3://odoo-conf/default.conf ./
 
 cd /home/ubuntu/nginx-data/ssl
- aws s3 cp s3://ssl-cert-hdap/ ./ --recursive
+aws s3 cp s3://ssl-cert-hdap/ ./ --recursive
 
 cd /home/ubuntu
 aws s3 cp s3://odoo-conf/docker-compose.yml ./
@@ -43,4 +44,5 @@ aws s3 cp s3://odoo-conf/docker-compose.yml ./
 #docker-compose up -d --force-recreate
 docker-compose up -d
 docker-compose exec web pip3 install paramiko
+docker-compose exec web pip3 install pykhipu
 docker-compose restart
